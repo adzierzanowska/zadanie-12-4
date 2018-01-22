798d0442332423b992104691199991828a4060b6
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-01-22 16:38:45 +0100

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ button.addEventListener('click', function(){
 
 var paragraph = document.getElementById('joke');
 
+
 function getJoke() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -16,3 +17,4 @@ function getJoke() {
     });
     xhr.send();
 }
+getJoke();
